2791f37b0db0cee6c3116f1075bbd79351273c36
Gabriel Pineda <willesden10@gmail.com>
2021-08-16 08:25:01 +0100

    Numbers Ok. Slightly misshapen

--- PascalT.java
+++ PascalT.java
@@ -2,7 +2,15 @@ public class PascalT{
     //Prints a Pascal's triangle with a variable number of rows.
     public static void main(String[] aStrings){
         //TODO: User input for the number of rows.
-        int rows = 8;
+        int rows = 10;
+
+        //Array to store the numers or each row.
+        int[] numbers = new int[rows];
+        numbers[0] = 1;
+
+        //Temporary array to calculate the current row's numbers from the previous row.
+        int[] temp = new int[rows];
+        temp[0] = 1;
 
         for(int row = 1; row <= rows; row++){
 
@@ -11,9 +19,19 @@ public class PascalT{
                 System.out.print(" ");
             }
 
-            //Prints 1s in each line to check that ge get a triangle
+            //Populating the temp array
+            for(int i=1; i<row; i++){
+                temp[i] += numbers[i-1];
+            }
+
+            //Copying the temp array into numbers.
+            for(int i=0; i<temp.length; i++){
+                numbers[i] = temp[i];
+            }
+
+            //Prints the row's numbers from the numbers arrray.
             for(int i=0; i<row; i++){
-                System.out.print("1 ");
+                System.out.print(numbers[i] + " ");
             }
 
             //Change or line.
---
+++ readme.md
@@ -0,0 +1,12 @@
+Prints out a pascal triangle.
+
+       0 
+      0 0 
+     0 0 0 
+    0 0 0 0 
+   0 0 0 0 0 
+  0 0 0 0 0 0 
+ 0 0 0 0 0 0 0 
+0 0 0 0 0 0 0 0 
+
+Work in progress...
